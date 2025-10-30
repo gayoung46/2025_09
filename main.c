f5387267de98ffff2850a4c6b4325ced0bc32737
gayoung46 <gayoung04066@naver.com>
2025-10-30 16:26:46 +0900

    09_5

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+     int i,j;
+     for(i=0;i<ROWS;i++)
+        for(j=0;j<COLS;j++)
+           C[i][j] = A[i][j] +B[i][j];
+     
+}
+
+void printMatrix(int A[][COLS])
+{
+     int i,j;
+     for(i=0;i<ROWS;i++)
+     {
+         for(j=0;j<COLS;j++)
+             printf("%3d", A[i][j]);
+         printf("\n");
+     }
+} 
+
 
 int main(int argc,char*argv[])
 {
-    int i;
-    int a[SIZE] = {1,2,3,4,5};
-    int b[SIZE] = {1,2,3,4,5};
-    int flag_same = 1;
+    int A[ROWS][COLS] ={
+        {2,3,0},
+        {8,9,1},
+        {7,0,5}};
     
-    for(i=0;i<SIZE;i++)
-    {
-        if(a[i] != b[i])
-        {
-                printf("%i-th element is diff\n", i); 
-                flag_same = 0;  
-        }
-    } 
+    int B[ROWS][COLS] ={
+        {1,0,0},
+        {0,1,0},
+        {0,0,1}};
     
-    printf("Are a and b the same? %i\n", flag_same);
+    int C[ROWS][COLS];
+    
+    addMatrix(A,B,C);
+    printMatrix(C);
     
     system("PAUSE");	
     return 0;
